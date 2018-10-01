e4219a4e223b79bb6585f059f33ed9aa3cfb4567
megustafather <neymeka_p@mail.ru>
2018-10-01 20:47:41 +0300

    sudoku

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  var checkRow = (row, matrix, candidate) => {
+  for (var i = 0; i < 9; i++) {
+    if (matrix[row][i] == candidate) {
+      return false;
+    };
+  };
+  return true;
+};
+
+var checkColumn = (column, matrix, candidate) => {
+  for (var i = 0; i < 9; i++) {
+    if (matrix[i][column] == candidate) {
+      return false;
+    };
+  };
+  return true;
+};
+
+var checkBox = (row, column, matrix, candidate) => {
+  var boxRowNumber = Math.floor((row / 3)) * 3;
+  var boxColumnNumber = Math.floor((column / 3)) * 3;
+  for (var boxRow = 0; boxRow < 3; boxRow++) {
+    for (var boxColumn = 0; boxColumn < 3; boxColumn++) {
+      if (matrix[boxRowNumber + boxRow][boxColumnNumber + boxColumn] == candidate) {
+        return false;
+      };
+    };
+  };
+  return true;
+};
+
+let solve = (matrix) => {
+  for (var row = 0; row <= 8; row++) {
+    for (var column = 0; column <= 8; column++) {
+      if (matrix[row][column] == 0) {
+        for (var candidate = 1; candidate <= 9; candidate++) {
+          if (checkBox(row, column, matrix, candidate)&&checkRow(row, matrix, candidate)&&checkColumn(column, matrix, candidate)) {
+            matrix[row][column] = candidate;
+            if (solve(matrix)) {
+              return true;
+            } else {
+              matrix[row][column] = 0;
+            };
+          };
+        };
+        return false;
+      };
+    };
+  };
+  return true;
+}
+
+solve(matrix);
+return matrix;
+// your solution
 }
